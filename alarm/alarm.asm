46a9a81b8c4e1e52caedc2413ca9f07997991bdf
Patrick Haußer <pat@binaerraum.de>
2018-04-02 09:57:51 +0200

    Ergaenze Alarm (Sleep+IRQ Wakeup)

--- alarm/alarm.asm
+++ alarm/alarm.asm
@@ -1,4 +1,4 @@
-; avra hello.asm && avrdude -p attiny85 -c usbasp -B 5 -U flash:w:hello.hex
+; avra alarm.asm && avrdude -p attiny85 -c usbasp -B 5 -U flash:w:alarm.hex
 
 .nolist
 .include "../inc/tn85def.inc"
@@ -8,6 +8,8 @@
 
 .org 0x0000
     rjmp init
+.org PCI0addr
+    rjmp pin
 
 init:
     ; Init Stack
@@ -20,17 +22,30 @@ init:
     ldi r16, (1 << DDB3)
     out DDRB, r16
 
+    ; Init IRQ
+    ldi r16, (1 << PCINT0)
+    out PCMSK, r16
+
+    ldi r16, (1 << PCIE)
+    out GIMSK, r16
+
+    sei
+
     ; Main Loop
 loop:
-    sbi PORTB, PORTB3
+    ;sbi PORTB, PORTB3
     rcall delay
-    cbi PORTB, PORTB3
+    ;cbi PORTB, PORTB3
     rcall delay
     rjmp loop
 
+pin:
+    sbi PORTB, PORTB3
+    reti
+
     ; Simple Delay
 delay:
-    ldi r22, 50
+    ldi r22, 100
 _delay_loop:
     dec r21
     brne _delay_loop
