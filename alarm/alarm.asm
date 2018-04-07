54116ea39b6049483b05bee79b23bae27aa63661
Patrick Haußer <pat@binaerraum.de>
2018-04-07 10:26:44 +0200

    Alarm wacht aus Sleep Mode auf

--- alarm/alarm.asm
+++ alarm/alarm.asm
@@ -1,5 +1,8 @@
 ; avra alarm.asm && avrdude -p attiny85 -c usbasp -B 5 -U flash:w:alarm.hex
 
+; 1) Power Down Modus
+; 2) Button = Blinken. 5 min oder bis Button gedrückt
+
 .nolist
 .include "../inc/tn85def.inc"
 .list
@@ -23,7 +26,7 @@ init:
     out DDRB, r16
 
     ; Init IRQ
-    ldi r16, (1 << PCINT0)
+    ldi r16, (1 << PCINT4)
     out PCMSK, r16
 
     ldi r16, (1 << PCIE)
@@ -33,22 +36,32 @@ init:
 
     ; Main Loop
 loop:
-    ;sbi PORTB, PORTB3
+    ldi
+    
+    ldi r16, (1 << SE) | (1 << SM1)
+    out MCUCR, r16
+
+    sleep
+
+    sbi PORTB, PORTB3
     rcall delay
-    ;cbi PORTB, PORTB3
-    rcall delay
+    cbi PORTB, PORTB3
     rjmp loop
 
 pin:
-    sbi PORTB, PORTB3
+    ;sbi PORTB, PORTB3
     reti
 
     ; Simple Delay
 delay:
-    ldi r22, 100
+    ldi r22, 200
 _delay_loop:
     dec r21
     brne _delay_loop
     dec r22
     brne _delay_loop
     ret
+
+.dseg
+count:
+        .byte 0
